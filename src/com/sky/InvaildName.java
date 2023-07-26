d96f9e1720d288e81e4bd3b46a08555e5095825c
Andrew-McCall <amccall@qa.com>
2023-07-26 16:17:48 +0100

    Lambda

--- src/com/sky/InvaildName.java
+++
@@ -1,9 +0,0 @@
-package com.sky;
-
-public class InvaildName extends Exception{
-
-    public InvaildName(){
-        super("Names can only have letters");
-    }
-
-}
--- src/com/sky/Main.java
+++ src/com/sky/Main.java
@@ -1,60 +1,66 @@
 package com.sky;
 
-import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
 
-    public static void main(String[] args) throws Exception {
-        // Enums*
-        // Inputs*
-        // Exceptions - Try and Catch*
+    public static void main(String[] args) {
         // Lambdas
-        // EOD Task
 
-//        throw new Exception();
+//        Integer[] numbersOne = {1,2,3,4,5}; // Prepopulated Arrays
+//        List<Integer> numbersTwo = new ArrayList<Integer>(List.of(new Integer[]{1, 2, 3, 4, 5})); // Can prepopulateArrayLists
 
-        // Person Object
-        // Static Method - public Person PersonForm() - Scanner with inputs
-        // Ext: 2Player Rock paper scissors
+        List<Integer> numbers = new ArrayList();
 
-        // A person Object must include
-        // Job Enum, age, name
-        // Checks for vaild inputs
-        // STATIC
-
-        // Ext: If the input is invaild, Messge why - Not to reset progress.
-
-//        Person p = Person.PersonCreator(); // static
-
-//        Person p = new Person();           // instance
-//        p.setup()
-
-        int b = 19;
-
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter a number");
-        try {
-            int a = sc.nextInt();
-
-            // %d integer
-            // %s String
-            // %f decimal number, float
-            // String.format("template", inputs)
-            System.out.println(String.format("%d / %d = %f", b , a, ((double) b) / a));
-            int check = b/a;
-
-        } catch (ArithmeticException e){
-            System.out.println("Arthimetic Error");
-
-        } catch (Exception e){
-            System.out.println("Execption");
-
-        } finally {
-            System.out.println("Always Run");
-
+        for (int i = 1; i < 10; i++) {
+            numbers.add(i*19);
         }
 
+        System.out.println(numbers);
 
+//        for (int i : numbers){
+//            System.out.println(i);
+//        }
+
+        // Lambda
+        numbers.forEach(x -> {x *= 2; System.out.println(x);});
+
+        numbers.forEach(i -> System.out.println(i));
+
+        int output = Operate(1420, 2014, (int x, int y) -> {return x * y;});
+        System.out.println(output);
+
+        output = Operate(666666, 2014, new Example());
+        System.out.println(output);
     }
 
+    public static int Operate(int a, int b, FuncInterface func){
+        return func.Operation(a, b);
+    }
+
+    // ArrayList.forEach
+    // Print every number
+    // Print only numbers divisible by 3
+
+    // Create a FuncInterface
+    // double Operation(double a, double b);
+    // Have a "Calculator Static Function"
+    // be used with 3 lamdas
+
+    // e.g int output = Operate(1420, 2014, (int x, int y) -> {return x * y;});
+    // Each lamda doing a different operation, return a+b, return a/b or return a*a+b*b
+
+    // Ext: Add to the Personform task
+}
+
+interface FuncInterface{
+    int Operation(int a, int b);
+}
+
+class Example implements  FuncInterface{
+    @Override
+    public int Operation(int a, int b) {
+        return a/b;
+    }
 }
